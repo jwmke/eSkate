6595a30d7ea699b9a2313503286cd102bdbcca28
Joseph Weller <joseph5062a@gmail.com>
2023-02-07 20:53:06 -0600

    map & color palette experimentation

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import MapView from 'react-native-maps';
+import { Ionicons } from '@expo/vector-icons';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import * as data from './ui/style/map.json';
 
 export default function App() {
+  const mapStyle = data.darkStyle;
+
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
-      <StatusBar style="auto" />
+      <MapView style={styles.map} customMapStyle={mapStyle}/>
+      <LinearGradient colors={['#fe4773', '#8f30a1']}
+       start={{ x: 1, y: 0 }}
+       end={{ x: 0, y: 1 }}
+       style={styles.searchBox}>
+        <Ionicons name="search" size={35} color="white"/>
+      </LinearGradient>
+      <StatusBar style="inverted" />
     </View>
   );
 }
@@ -13,8 +26,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  map: {
+    width: '100%',
+    height: '100%',
+    zIndex: 1,
+  },
+  searchBox: {
+    height: 55,
+    width: 55,
+    top: 45,
+    right: 15,
+    position: "absolute",
+    backgroundColor: "#fff",
+    zIndex: 2,
+    borderRadius: 10,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
 });
